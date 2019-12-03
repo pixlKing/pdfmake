253e9cafedb916adbe5bfe82851223c5030cdf90
PixlKing <campagnamatias@gmail.com>
2019-12-03 17:33:40 -0300

    .

--- test.js
+++ test.js
@@ -1,3 +1,25 @@
 function generarPDF(){
-	alert("asdadsa");
+
+	var numeroLicencia = 12345678;
+
+	var docDefinition = {
+	  content: [
+	    {
+	      table: {
+	        widths: [ '*', '*', '*', ],
+	        //heights: 40,
+	        body: [
+	        	[
+	        		{text:'Transener', alignment: 'center'},
+	        		{text: 'Licencia de trabajo', alignment: 'center'},
+	        		{text: numeroLicencia, alignment: 'center'}
+	        	]
+	        ]
+	      }
+	    }
+	  ]
+	};
+
+	pdfMake.createPdf(docDefinition).download();
 }
+
